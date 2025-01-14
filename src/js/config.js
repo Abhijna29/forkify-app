5b0aa5c6abfb3e54dd3bd02974582460cea9a152
Abhijna S S <abhijnas.shettigar@gmail.com>
2025-01-14 10:26:41 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '28d4f9c0-45cf-45b6-8cce-892cf1853bce';
+export const MODAL_CLOSE_SEC = 2.5;
